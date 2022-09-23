02b6930e2a204068fb79d95a2ef50a0b3b3b2c61
Facundo Sichi <113444008+FSichi-Callao@users.noreply.github.com>
2022-09-22 23:17:24 -0300

    Version v2 Interfaz Grafica Consola Paas

---
+++ src/pages/UserList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DonoutGraf } from '../components/DonoutGraf/DonoutGraf'
+
+export const UserList = () => {
+    return (
+        <div className='p-4'>
+            <DonoutGraf />
+        </div>
+    )
+}
---
+++ src/routes/Main.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { SidebarDrop } from '../components/SidebarDrop/SidebarDrop';
+import { Header } from '../components/Header/Header';
+/* PAGES */
+import { Dashboard } from '../pages/Dashboard';
+import { UserList } from '../pages/UserList';
+
+import '../App.css'
+
+export const Main = () => {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+    const handleOpenSidebar = (id) => {
+        (id === 1) ? setIsSidebarOpen(!isSidebarOpen) : setIsSidebarOpen(true);
+    }
+
+    return (
+        <BrowserRouter>
+
+            <SidebarDrop valueOpen={isSidebarOpen} handleOpen={handleOpenSidebar} />
+
+            <section className="home-section">
+
+                <Header handleOpenSidebar={handleOpenSidebar} />
+
+                <div className='page-content'>
+                    <Routes>
+                        <Route exact path='/dashboard' element={<Dashboard />}></Route>
+                        <Route exact path='/accounts' element={<UserList />} />
+                    </Routes>
+                </div>
+
+            </section>
+
+        </BrowserRouter>
+    )
+}
